f0bbc14cc24ee531c836af8f9ae877833a927f33
canercanbaz <caner@cnrcnbz.com>
2018-10-16 23:38:52 +0300

    added a new property

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.json({ name: 'Caner', surname: 'Canbaz', age: 24 });
+	res.json({ name: 'Caner', surname: 'Canbaz', age: 24, location: 'Ankara' });
 });
 
 module.exports = router;
